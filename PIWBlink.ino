b971e1de598cddd92e6984b54a96965a2745d698
Supawadee <supawadee018@gmail.com>
2018-01-24 11:55:29 +0700

    Finish

--- PIWBlink.ino
+++ PIWBlink.ino
@@ -1,6 +1,8 @@
 //ส่วนของการประกาศตัวแปร
-int myOutput = D0;
-int myDelay=2000;
+int myOutput = D7;
+int myOutput2 = D8;
+
+int myDelay=3000;//ค่าของเวลาจะมีหน่วยเป็น มิลลิวินาที
 
 
 
@@ -10,15 +12,27 @@ void setup() {
 
 //  ส่วนที่กำหนดการส่งสัญญาณ digital Out จากขาของ Node
   pinMode(myOutput, OUTPUT);
-
+  pinMode(myOutput2, OUTPUT);
 
 
 }
 
 // ส่วนของ loop จะทำงาน วนไปมา ตามสัญญาณนาฬิกา Digital Logic ตลอดเวลา
 void loop() {
+
+//  for myOutput
   digitalWrite(myOutput, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(myDelay);                       // wait for a second
   digitalWrite(myOutput, LOW);    // turn the LED off by making the voltage LOW
+  delay(myDelay); 
+
+  //  for myOutput2
+  digitalWrite(myOutput2, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(myDelay);                       // wait for a second
-}
+  digitalWrite(myOutput2, LOW);    // turn the LED off by making the voltage LOW
+  delay(myDelay); 
+  
+  
+  
+  
+}//end of Loop
